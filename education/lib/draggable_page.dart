378b4ef61e227d1d8eb01fd095e091f2dab69615
張璋 <>
2020-05-03 23:33:05 +0900

    フォーマットする

--- education/lib/draggable_page.dart
+++ education/lib/draggable_page.dart
@@ -25,31 +25,29 @@ class _DraggablePageState extends State<DraggablePage> {
         // 内容
         slivers: <Widget>[
           new SliverPadding(
-            padding: const EdgeInsets.only(left: 0,right: 0),
+            padding: const EdgeInsets.only(left: 0, right: 0),
             sliver: new SliverList(
               delegate: new SliverChildListDelegate(
                 <Widget>[
-
                   Row(
                     children: <Widget>[
-
                       Container(
-                      padding: const EdgeInsets.only(left: 13,top: 12,bottom: 7),
-                      child: Text(
-                        "予定項目",
-                        style: TextStyle(
-                          fontFamily: "ヒラギノ角ゴ ProN W3",
-                            fontSize:24.0,
-                            color: Colors.black),
+                        padding:
+                            const EdgeInsets.only(left: 13, top: 12, bottom: 7),
+                        child: Text(
+                          "予定項目",
+                          style: TextStyle(
+                              fontFamily: "ヒラギノ角ゴ ProN W3",
+                              fontSize: 24.0,
+                              color: Colors.black),
+                        ),
                       ),
-                    ),
                     ],
                   ),
                   Column(
                     children: <Widget>[
-
                       Container(
-                        child:GestureDetector(
+                        child: GestureDetector(
                           child: Container(
                             height: 248,
                             width: MediaQuery.of(context).size.width - 26,
@@ -64,10 +62,10 @@ class _DraggablePageState extends State<DraggablePage> {
                             ),
                             child: DragTarget<String>(
                               builder: (
-                                  BuildContext context,
-                                  List<dynamic> accepted,
-                                  List<dynamic> rejected,
-                                  ) {
+                                BuildContext context,
+                                List<dynamic> accepted,
+                                List<dynamic> rejected,
+                              ) {
                                 return _createGridView(_items1);
                               },
                               onAccept: (String data) {
@@ -81,16 +79,16 @@ class _DraggablePageState extends State<DraggablePage> {
                         ),
                       ),
 
-
                       Row(
                         children: <Widget>[
                           Container(
-                            padding: const EdgeInsets.only(left: 13,top: 12,bottom: 7),
+                            padding: const EdgeInsets.only(
+                                left: 13, top: 12, bottom: 7),
                             child: Text(
                               "復習項目",
                               style: TextStyle(
                                   fontFamily: "ヒラギノ角ゴ ProN W3",
-                                  fontSize:24.0,
+                                  fontSize: 24.0,
                                   color: Colors.black),
                             ),
                           ),
@@ -100,7 +98,7 @@ class _DraggablePageState extends State<DraggablePage> {
                       //SizedBox(height: 210),
 
                       Container(
-                        child:GestureDetector(
+                        child: GestureDetector(
                           child: Container(
                             height: 248,
                             width: MediaQuery.of(context).size.width - 26,
@@ -115,10 +113,10 @@ class _DraggablePageState extends State<DraggablePage> {
                             ),
                             child: DragTarget<String>(
                               builder: (
-                                  BuildContext context,
-                                  List<dynamic> accepted,
-                                  List<dynamic> rejected,
-                                  ) {
+                                BuildContext context,
+                                List<dynamic> accepted,
+                                List<dynamic> rejected,
+                              ) {
                                 return _createGridView(_items2);
                               },
                               onAccept: (String data) {
@@ -132,16 +130,16 @@ class _DraggablePageState extends State<DraggablePage> {
                         ),
                       ),
 
-
                       Row(
                         children: <Widget>[
                           Container(
-                            padding: const EdgeInsets.only(left: 13,top: 12,bottom: 7),
+                            padding: const EdgeInsets.only(
+                                left: 13, top: 12, bottom: 7),
                             child: Text(
                               "終了項目",
                               style: TextStyle(
                                   fontFamily: "ヒラギノ角ゴ ProN W3",
-                                  fontSize:24.0,
+                                  fontSize: 24.0,
                                   color: Colors.black),
                             ),
                           ),
@@ -149,7 +147,7 @@ class _DraggablePageState extends State<DraggablePage> {
                       ),
 
                       Container(
-                        child:GestureDetector(
+                        child: GestureDetector(
                           child: Container(
                             height: 248,
                             width: MediaQuery.of(context).size.width - 26,
@@ -164,10 +162,10 @@ class _DraggablePageState extends State<DraggablePage> {
                             ),
                             child: DragTarget<String>(
                               builder: (
-                                  BuildContext context,
-                                  List<dynamic> accepted,
-                                  List<dynamic> rejected,
-                                  ) {
+                                BuildContext context,
+                                List<dynamic> accepted,
+                                List<dynamic> rejected,
+                              ) {
                                 return _createGridView(_items3);
                               },
                               onAccept: (String data) {
@@ -184,74 +182,69 @@ class _DraggablePageState extends State<DraggablePage> {
                       Row(
                         children: <Widget>[
                           Container(
-                            padding: const EdgeInsets.only(top: 18,left: 13),
-                            child:pictureTwitter('images/twitter.png'),
+                            padding: const EdgeInsets.only(top: 18, left: 13),
+                            child: pictureTwitter('images/twitter.png'),
                           ),
                           Container(
                             padding: const EdgeInsets.only(top: 18),
                             //「Twitterで共有」ボタン
-                            child:picAndTextButton('images/twitter_back_image.png', "images/twitter_common.png"),
+                            child: picAndTextButton(
+                                'images/twitter_back_image.png',
+                                "images/twitter_common.png"),
                           ),
                         ],
                       ),
                     ],
                   ),
-
-Container(
-  padding: const EdgeInsets.only(top: 31,left: 13),
-  child: Text(
-    "フッターメニュー",
-    style: TextStyle(
-        fontFamily: "ヒラギノ角ゴ ProN W3",
-        fontWeight: FontWeight.bold,
-        fontSize:20.0,
-        color: Color.fromRGBO(203, 0, 0, 1)),
-  ),
-),
-Container(
-  padding: const EdgeInsets.only(left: 0,right: 0,top: 2.5),
-  child:Divider(
-
-    height: 2.0,
-    //indent: 0.0,
-    color: Colors.black,
-  ),
-),
-
+                  Container(
+                    padding: const EdgeInsets.only(top: 31, left: 13),
+                    child: Text(
+                      "フッターメニュー",
+                      style: TextStyle(
+                          fontFamily: "ヒラギノ角ゴ ProN W3",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Color.fromRGBO(203, 0, 0, 1)),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 0, right: 0, top: 2.5),
+                    child: Divider(
+                      height: 2.0,
+                      //indent: 0.0,
+                      color: Colors.black,
+                    ),
+                  ),
                   Container(
                     child: Row(
                       children: <Widget>[
-
                         Container(
-                          width:80,
+                          width: 80,
                           padding: const EdgeInsets.only(top: 17),
                           child: footerMenu("本日の\nタスク", test),
                         ),
                         Container(
                           padding: const EdgeInsets.only(top: 17),
-                          width:70,
+                          width: 70,
                           child: footerMenu("予定\n管理", test),
                         ),
-
                         Container(
                           padding: const EdgeInsets.only(top: 17),
-                          width:70,
+                          width: 70,
                           child: footerMenu("目標\n管理", test),
                         ),
                         Container(
-                          padding: const EdgeInsets.only(top: 17,right: 10),
-                          width:80,
+                          padding: const EdgeInsets.only(top: 17, right: 10),
+                          width: 80,
                           child: footerMenu("連絡", test),
                         ),
-Container(
-
-  width:100,
-child: footerMenu("メニュー", test),
-),
+                        Container(
+                          width: 100,
+                          child: footerMenu("メニュー", test),
+                        ),
                       ],
                     ),
                   ),
-
                   SizedBox(height: 20),
                 ],
               ),
@@ -263,67 +256,58 @@ child: footerMenu("メニュー", test),
   }
 
   Widget pictureTwitter(String imgpath) {
-
     return Container(
       width: 41,
       height: 31,
       decoration: BoxDecoration(
         color: Colors.white,
-        image: DecorationImage(
-            image: AssetImage(imgpath),
-            fit: BoxFit.fill
-        ),
+        image: DecorationImage(image: AssetImage(imgpath), fit: BoxFit.fill),
       ),
     );
   }
 
-  Widget footerMenu(String text,VoidCallback onPressed){
+  Widget footerMenu(String text, VoidCallback onPressed) {
     return Container(
-      child:FlatButton(
+      child: FlatButton(
         onPressed: onPressed,
         child: Text(
           text,
           style: TextStyle(
               fontFamily: "ヒラギノ角ゴ ProN W3",
-              fontSize:16.0,
+              fontSize: 16.0,
               fontWeight: FontWeight.bold,
               color: Colors.black),
-
         ),
       ),
     );
   }
-  Widget picAndTextButton(String imagePathOne,String imagePathTwo) {
 
+  Widget picAndTextButton(String imagePathOne, String imagePathTwo) {
     return Container(
       width: 119,
       height: 31,
       decoration: BoxDecoration(
         color: Colors.white,
-        image: DecorationImage(
-            image: AssetImage(imagePathOne),
-            fit: BoxFit.fill
-        ),
+        image:
+            DecorationImage(image: AssetImage(imagePathOne), fit: BoxFit.fill),
       ),
-      child:Container(
-        padding: const EdgeInsets.only(left: 0,right: 0,top: 8,bottom: 7),
-        child:FlatButton(
+      child: Container(
+        padding: const EdgeInsets.only(left: 0, right: 0, top: 8, bottom: 7),
+        child: FlatButton(
           onPressed: () async {
-            FlutterShareGo.shareToTwitterPlatform("test share to twitter content", "http://tryenough.com");
+            FlutterShareGo.shareToTwitterPlatform(
+                "test share to twitter content", "http://tryenough.com");
           },
           child: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage(imagePathTwo),
-                  fit: BoxFit.fill
-              ),
+                  image: AssetImage(imagePathTwo), fit: BoxFit.fill),
             ),
           ),
         ),
       ),
     );
   }
-
 
   test() {
     print("item");
@@ -358,7 +342,7 @@ child: footerMenu("メニュー", test),
             alignment: Alignment.center,
             foregroundDecoration: BoxDecoration(color: Colors.white30),
             child: Text(
-             // _items[index],
+              // _items[index],
               "数学" + "  " + "チャートIA 場合の数 例題" + "  " + "13",
               style: TextStyle(color: Colors.white),
             ),
@@ -370,7 +354,7 @@ child: footerMenu("メニュー", test),
               color: Colors.blueAccent,
               alignment: Alignment.center,
               child: Text(
-               // _items[index],
+                // _items[index],
                 "数学" + "  " + "チャートIA 場合の数 例題" + "  " + "13",
                 style: TextStyle(color: Colors.white),
               ),
@@ -383,7 +367,7 @@ child: footerMenu("メニュー", test),
               color: Colors.blueAccent,
               alignment: Alignment.center,
               child: Text(
-               // _items[index],
+                // _items[index],
                 "数学" + "  " + "チャートIA 場合の数 例題" + "  " + "13",
                 style: TextStyle(color: Colors.white),
               ),
